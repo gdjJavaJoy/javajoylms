e939267d26c98e375000ab3f69f812c3c050d238
orion174 <orion174@naver.com>
2022-06-21 17:22:51 +0900

    6/21 [박범진] 과제 게시판 Mapper, Service, Controller 파일 생성

---
+++ lms/src/main/java/kr/co/javajoy/lms/controller/SubjectReportController.java
@@ -0,0 +1,5 @@
+package kr.co.javajoy.lms.controller;
+
+public class SubjectReportController {
+	// 박범진 작업예쩡
+}
---
+++ lms/src/main/java/kr/co/javajoy/lms/service/SubjectReportService.java
@@ -0,0 +1,5 @@
+package kr.co.javajoy.lms.service;
+
+public class SubjectReportService {
+	// 박범진 작업예정
+}
